d4dd6e27e48173d646ab0c85c47cfd321c9cd511
ingfaraya <162543145+ingfaraya@users.noreply.github.com>
2024-11-18 01:33:48 -0300

    ajustes recetas

--- frontend-libros/src/app/app.routes.ts
+++ frontend-libros/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'libros', pathMatch: 'full' },
+  { path: '', redirectTo: 'recetas', pathMatch: 'full' },
   { path: 'recetas', component: RecetasComponent },
   { path: 'libros', component: LibrosComponent },
   { path: 'login', component: LoginComponent }
