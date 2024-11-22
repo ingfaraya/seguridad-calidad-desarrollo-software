69c8945e575af9b0cfe0721eb171d4e36cc2d49f
ingfaraya <162543145+ingfaraya@users.noreply.github.com>
2024-11-22 00:10:37 -0300

    limpieza cache - docker - recetas

--- frontend-recetas/src/main.ts
+++ frontend-recetas/src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
